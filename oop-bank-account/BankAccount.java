026d7fe800f2a0b66d90e1fa2764a4f107609a44
Adam Ghebrenegus <ghebrenegusab@cardiff.ac.uk>
2023-10-03 07:49:51 +0100

    Project Skeleton

---
+++ oop-bank-account/BankAccount.java
@@ -0,0 +1,43 @@
+abstract class BankAccount{
+    // constructor, deposit, withdraw, check balance, calculate interest (savings), transfer funds, close account, loan request, change password , view transaction history, 
+    private String accountHolderName;
+    private double accountBalance;
+    private int accountNumber; 
+    private int pin;
+
+    // constructor function
+    public BankAccount(String _accountHolderName, int _accountNumber, int _pin){
+        this.accountHolderName = _accountHolderName;
+        this.accountNumber = _accountNumber;
+        this.pin = _pin;
+    }
+
+    public void deposit(double amount){
+        this.accountBalance += amount;
+    }
+
+    public void withdraw(double amount){
+        if (this.accountBalance < amount){
+            System.out.println("Rejected");
+        }else{
+            this.accountBalance -= amount;
+        }
+    }
+
+    public void transferFunds(int accountNumber, double amount){
+        
+    };
+
+    // getter function
+    public Int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    public double getAccountBalance(){
+        return this.accountBalance;
+    }
+
+    public void getAccountInformation(){
+        // print out account information here
+    }
+}
---
+++ oop-bank-account/BankingSystem.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class BankingSystem{
+    public static void main(String[] args){
+        // list of accounts
+        ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+        // User Interface
+
+        
+    }
+}
